ef57f293e133e0315befa737cf21005b293ad358
MichPAz <31188522+MichPAz@users.noreply.github.com>
2017-10-25 16:43:22 -0400

    Chore: grafoCaminho, falta eliminha a multiplicidade

--- grafoCaminho.cpp
+++ grafoCaminho.cpp
@@ -8,7 +8,7 @@ bool inRange(int i, int j)
     return ( (i>=0) &&(i<8) && (j >=0) && (j<8) );
 }
 
-vector<int> movCavaleiro(int p)
+vector<int> movL(int p)
 {
     vector <int> pos;
     int tabuleiro[8][8];
@@ -27,7 +27,7 @@ vector<int> movCavaleiro(int p)
     }
 
     if(!inRange(x,y))
-        cout << "posicao invalida" ;
+        cout << "posicao invalida, erro na posicao do tabuleiro." ;
 
     if(inRange(x+2,y+1))
         pos.push_back(tabuleiro[x+2][y+1]);
@@ -46,26 +46,46 @@ vector<int> movCavaleiro(int p)
     if(inRange(x-1,y-2))
         pos.push_back(tabuleiro[x-1][y-2]);
 
-    for(int i=0;i<8;i++){
+    /*for(int i=0;i<8;i++){
         cout << "\n";
         for(int j=0;j<8;j++){
                 cout << "   " << tabuleiro[i][j] << "   ";
         }
-    }
-    cout << "\n\n";
+    }*/
+    /*cout << "\n\n";
     for(int i=0;i<pos.size();i++){
         cout << pos.at(i) <<endl;
-    }
+    }*/
 
     return pos;
 
 }
 
-/*Graph geraCaminhosCavaleiro(int posCav){
-
-}*/
+Graph geraCaminhos(int posInicial){
+    int visited[64];
+    for(int i=0;i<64;i++){
+        visited[i]=0;
+    }
+    //visited[posInicial]=true;
+    Graph graph(64);
+    Fila<int> q;
+    q.enfileira(posInicial);
+    while(!q.vazia()){
+        int u = q.desenfileira();
+        for(int i = 0; i < movL(u).size();i++){
+            if(visited[movL(u).at(i)]<movL(movL(u).at(i)).size()){
+                graph.insertEdge(u, movL(u).at(i));
+                visited[movL(u).at(i)]++;
+                cout<<"enfileira " << movL(u).at(i)<<endl;
+                q.enfileira(movL(u).at(i));
+            }
+        }
+    }
+    graph.print();
+    return graph;
+}
 
 int main(){
-    movCavaleiro(59);
+    geraCaminhos(1);
     return 0;
 }
